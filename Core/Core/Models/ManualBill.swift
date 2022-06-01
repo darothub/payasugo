cf5a0208927e0a281a29607f79d3a8fbcc0ae5e1
adurosomo <durosomo.abdulrasaq@cellulant.io>
2022-06-01 21:01:18 +0100

    added static fields to category

--- Core/Core/Models/ManualBill.swift
+++ Core/Core/Models/ManualBill.swift
@@ -18,6 +18,15 @@ public class ManualBill : Identifiable, Codable {
     public let categoryId: String? = ""
     public let accountPayload: String? = nil
     public let category: Category? = nil
+    public static let PARKING_ID = "24"
+    public static let PAY_TV_ID = "1"
+    public static let POWER_ID = "10"
+    public static let INTERNET_ID = "14"
+    public static let WATER_ID = "11"
+    public static let AIRTIME_ID = "2"
+    public static let GAS_ID = "22"
+    public static let OTHER_ID = "8"
+    public static let INVEST_ID = "4"
     enum CodingKeys: String, CodingKey {
         case id = "MANUAL_BILL_ID"
         case accountNumber = "MERCHANT_ACCOUNT_NUMBER"
