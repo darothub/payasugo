b2775c2c5861de00f56d159d20916f133634b6de
adurosomo <durosomo.abdulrasaq@cellulant.io>
2022-06-02 10:17:39 +0100

    modified contact and manualbill models

--- Core/Core/Models/Contact.swift
+++ Core/Core/Models/Contact.swift
@@ -7,31 +7,40 @@
 
 import Foundation
 
-public class Contact : Identifiable, Encodable {
-    public let id: Int64 = 0
-    public let email: String? = ""
-    public let facebookUrl: String? = ""
-    public let phone: String? = ""
-    public let googlePlayUrl: String? = ""
-    public let twitterUrl: String? = ""
-    public let countryID: String? = ""
-    public let website: String? = ""
-    public let address: String? = ""
-    public let location: String? = ""
-    public let title: String? = ""
-    public let dateCreate: String = ""
+// MARK: - Contact
+public class Contact: Identifiable, Codable {
+    public let id: Int
+    public let email, facebook, phone, link: String?
+    public let twitter, countryID, website, address: String?
+    public let location, contactTitle, dateCreate: String?
+
     enum CodingKeys: String, CodingKey {
-        case id = "CONTACTS_ID"
+        case id
         case email = "EMAIL"
-        case facebookUrl = "FACEBOOK"
+        case facebook = "FACEBOOK"
         case phone = "PHONE"
-        case googlePlayUrl = "LINK"
-        case twitterUrl = "TWITTER"
+        case link = "LINK"
+        case twitter = "TWITTER"
         case countryID = "COUNTRY_ID"
         case website = "WEBSITE"
         case address = "ADDRESS"
         case location = "LOCATION"
-        case title = "CONTACT_TITLE"
-        
+        case contactTitle = "CONTACT_TITLE"
+        case dateCreate
+    }
+
+    init(id: Int, email: String?, facebook: String?, phone: String?, link: String?, twitter: String?, countryID: String?, website: String?, address: String?, location: String?, contactTitle: String?, dateCreate: String?) {
+        self.id = 0
+        self.email = email
+        self.facebook = facebook
+        self.phone = phone
+        self.link = link
+        self.twitter = twitter
+        self.countryID = countryID
+        self.website = website
+        self.address = address
+        self.location = location
+        self.contactTitle = contactTitle
+        self.dateCreate = dateCreate
     }
 }
--- Core/Core/Models/ManualBill.swift
+++ Core/Core/Models/ManualBill.swift
@@ -6,38 +6,48 @@
 //
 
 import Foundation
-public class ManualBill : Identifiable, Codable {
-    public let id: String = ""
-    public let accountNumber: String? = nil
-    public let merchantAccountName: String? = nil
-    public let paymentFrequency: String? = nil
-    public let amount: String? = nil
-    public let payerOptions: [PayerOptions]? = nil
-    public let billDueDate: String? = nil
-    public let manualBillType: String? = nil
-    public let categoryId: String? = ""
-    public let accountPayload: String? = nil
-    public let category: Category? = nil
-    public static let PARKING_ID = "24"
-    public static let PAY_TV_ID = "1"
-    public static let POWER_ID = "10"
-    public static let INTERNET_ID = "14"
-    public static let WATER_ID = "11"
-    public static let AIRTIME_ID = "2"
-    public static let GAS_ID = "22"
-    public static let OTHER_ID = "8"
-    public static let INVEST_ID = "4"
+
+// MARK: - ManualBil
+public class ManualBil: Codable {
+    public let manualBillID, merchantAccountNumber, merchantName, billReminderFrequency: String?
+    public let billAmount: String?
+    public let paymentOptions: [PayerOptions]?
+    public let billDueDate, isBillSearch, categoryID, accountPayload: String?
+    public let category: Category?
+//    public static let PARKING_ID = "24"
+//    public static let PAY_TV_ID = "1"
+//    public static let POWER_ID = "10"
+//    public static let INTERNET_ID = "14"
+//    public static let WATER_ID = "11"
+//    public static let AIRTIME_ID = "2"
+//    public static let GAS_ID = "22"
+//    public static let OTHER_ID = "8"
+//    public static let INVEST_ID = "4"
+
     enum CodingKeys: String, CodingKey {
-        case id = "MANUAL_BILL_ID"
-        case accountNumber = "MERCHANT_ACCOUNT_NUMBER"
-        case merchantAccountName = "MERCHANT_NAME"
-        case paymentFrequency = "BILL_REMINDER_FREQUENCY"
-        case amount = "BILL_AMOUNT"
-        case payerOptions = "PAYMENT_OPTIONS"
+        case manualBillID = "MANUAL_BILL_ID"
+        case merchantAccountNumber = "MERCHANT_ACCOUNT_NUMBER"
+        case merchantName = "MERCHANT_NAME"
+        case billReminderFrequency = "BILL_REMINDER_FREQUENCY"
+        case billAmount = "BILL_AMOUNT"
+        case paymentOptions = "PAYMENT_OPTIONS"
         case billDueDate = "BILL_DUE_DATE"
-        case manualBillType = "IS_BILL_SEARCH"
-        case categoryId = "CATEGORY_ID"
-        case accountPayload = "accountPayload"
-        case category = " category_"
+        case isBillSearch = "IS_BILL_SEARCH"
+        case categoryID = "CATEGORY_ID"
+        case accountPayload, category
+    }
+
+    init(manualBillID: String?, merchantAccountNumber: String?, merchantName: String?, billReminderFrequency: String?, billAmount: String?, paymentOptions: [PayerOptions]?, billDueDate: String?, isBillSearch: String?, categoryID: String?, accountPayload: String?, category: Category?) {
+        self.manualBillID = manualBillID
+        self.merchantAccountNumber = merchantAccountNumber
+        self.merchantName = merchantName
+        self.billReminderFrequency = billReminderFrequency
+        self.billAmount = billAmount
+        self.paymentOptions = paymentOptions
+        self.billDueDate = billDueDate
+        self.isBillSearch = isBillSearch
+        self.categoryID = categoryID
+        self.accountPayload = accountPayload
+        self.category = category
     }
 }
