a70640e6a9a06124156050d53151cce0b666aed3
adurosomo <durosomo.abdulrasaq@cellulant.io>
2022-06-09 13:22:16 +0100

    added camera permission and launcher

---
+++ Permissions/Permissions/Camera/CameraLauncherView.swift
@@ -0,0 +1,8 @@
+//
+//  CameraLauncherView.swift
+//  Permissions
+//
+//  Created by Abdulrasaq on 09/06/2022.
+//
+
+import Foundation
---
+++ Permissions/Permissions/Permissions.h
@@ -0,0 +1,18 @@
+//
+//  Permissions.h
+//  Permissions
+//
+//  Created by Abdulrasaq on 09/06/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Permissions.
+FOUNDATION_EXPORT double PermissionsVersionNumber;
+
+//! Project version string for Permissions.
+FOUNDATION_EXPORT const unsigned char PermissionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Permissions/PublicHeader.h>
+
+
