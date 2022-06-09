7bf813e1d15b1a1c02382fca84eb7b649ecd0d0f
adurosomo <durosomo.abdulrasaq@cellulant.io>
2022-06-09 23:54:27 +0100

    check camera availability

---
+++ Permissions/Permissions/Camera/CameraError.swift
@@ -0,0 +1,8 @@
+//
+//  CameraError.swift
+//  Permissions
+//
+//  Created by Abdulrasaq on 09/06/2022.
+//
+
+import Foundation
---
+++ Permissions/Permissions/Camera/CameraManager.swift
@@ -0,0 +1,8 @@
+//
+//  CameraManager.swift
+//  Permissions
+//
+//  Created by Abdulrasaq on 09/06/2022.
+//
+
+import Foundation
